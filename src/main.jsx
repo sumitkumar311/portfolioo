b052c4b13af7bdaac882120aa7554b79422a742e
Sumit Kumar <sumitshrivaa44@gmail.com>
2025-05-06 12:31:38 +0530

    Fix router for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter as BrowseRouter } from "react-router-dom";
+// import { BrowserRouter as BrowseRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 import 'remixicon/fonts/remixicon.css';
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowseRouter>
+    <HashRouter>
       <App />
-    </BrowseRouter>
+    </HashRouter>
   </StrictMode>
 );
